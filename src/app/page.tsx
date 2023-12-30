e2994c9dc41d59da498e4585c0de26f34ed06626
Luci Lua <luci.lua81@gmail.com>
2023-12-30 18:26:20 -0300

    features + prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,55 @@ export default function homepage() {
         <h1 className="text-[35px] font-[700] ">Projetos</h1>
         <h2>Alguns projetos em destaque</h2>
         <div className="my-[40px] flex w-full justify-center gap-12 flex-wrap transform perspective-3d">
-          <Projeto name={"Hidrofobia"} img={"/assets/projects/hidrofobia.png"} url={"https://www.curtahidrofobia.com/"} stack={["reactJS", "nextJS", "tailwindcss"]}/>
-          <Projeto name={"Somuuh"} img={"/assets/projects/somuuh.png"} url={"https://www.somuuh.vercel.app/"} stack={["reactJS", "nextJS", "tailwindcss", "next-auth", "typescript"]}/>
-          <Projeto name={"Notein"} img={"/assets/projects/notein.png"} url={"https://notein.vercel.app/"} stack={["reactJS", "nextJS", "sass", "mongoDB", "typescript", "axios"]}/>
-          <Projeto name={"Pokedex"} img={"/assets/projects/pokedex.png"} url={"https://pokedeex.vercel.app/"} stack={["reactJS", "nextJS", "sass"]}/>
-          <Projeto name={"ToDo"} img={"/assets/projects/todo.png"} url={"https://todo-lucilua.vercel.app/"} stack={["reactJS", "nextJS", "sass", "typescript", "localStorage"]}/>
-          <Projeto name={"Compare"} img={"/assets/projects/compare.png"} url={"https://compare-orpin.vercel.app/"} stack={["reactJS", "nextJS", "sass", "typescript", "axios"]}/>
+          <Projeto
+            name={"Hidrofobia"}
+            img={"/assets/projects/hidrofobia.png"}
+            url={"https://www.curtahidrofobia.com/"}
+            stack={["reactJS", "nextJS", "tailwindcss"]}
+          />
+          <Projeto
+            name={"Somuuh"}
+            img={"/assets/projects/somuuh.png"}
+            url={"https://www.somuuh.vercel.app/"}
+            stack={[
+              "reactJS",
+              "nextJS",
+              "tailwindcss",
+              "next-auth",
+              "typescript",
+            ]}
+          />
+          <Projeto
+            name={"Notein"}
+            img={"/assets/projects/notein.png"}
+            url={"https://notein.vercel.app/"}
+            stack={[
+              "reactJS",
+              "nextJS",
+              "sass",
+              "mongoDB",
+              "typescript",
+              "axios",
+            ]}
+          />
+          <Projeto
+            name={"Pokedex"}
+            img={"/assets/projects/pokedex.png"}
+            url={"https://pokedeex.vercel.app/"}
+            stack={["reactJS", "nextJS", "sass"]}
+          />
+          <Projeto
+            name={"ToDo"}
+            img={"/assets/projects/todo.png"}
+            url={"https://todo-lucilua.vercel.app/"}
+            stack={["reactJS", "nextJS", "sass", "typescript", "localStorage"]}
+          />
+          <Projeto
+            name={"Compare"}
+            img={"/assets/projects/compare.png"}
+            url={"https://compare-orpin.vercel.app/"}
+            stack={["reactJS", "nextJS", "sass", "typescript", "axios"]}
+          />
         </div>
       </div>
       <div className="mx-auto w-full max-w-[1300px] py-[20px] px-[10px]">
--- src/components/Projeto.tsx
+++ src/components/Projeto.tsx
@@ -1,15 +1,15 @@
 "use client"
 
 import { motion, useInView } from "framer-motion"
-import { useRef } from "react";
+import { useRef } from "react"
 
 export function Projeto({ name, img, url, stack }) {
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: false })
 
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: false });
-
-
-  const rotated = `${["translate3d(20px, -10px, 5px)"+"rotateY(20deg)"+"rotateX(20deg)"]}`
+  const rotated = `${[
+    "translate3d(20px, -10px, 5px)" + "rotateY(20deg)" + "rotateX(20deg)",
+  ]}`
 
   return (
     <motion.div
@@ -18,19 +18,37 @@ export function Projeto({ name, img, url, stack }) {
       style={{
         transform: isInView ? "none" : rotated,
         opacity: isInView ? 1 : 0,
-        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
       }}
     >
-      <img src={img} alt="image_project" className="w-full object-contain h-fit opacity-90  grayscale-[0.3] contrast-[1.1] brightness-[0.95]" />
+      <img
+        src={img}
+        alt="image_project"
+        className="w-full object-contain h-fit opacity-90  grayscale-[0.3] contrast-[1.1] brightness-[0.95]"
+      />
       <div className="flex w-full gap-2 h-fit border-y-[1px] border-y-[#ddd] px-[4px] py-[10px] flex-wrap">
-        {stack.map(tool => {
-          return (<div className="bg-[#ebebeb] px-[8px] py-[3px] flex text-[13px] font-[500] rounded-[4px]">{tool}</div>)
+        {stack.map((tool) => {
+          return (
+            <div className="bg-[#ebebeb] px-[8px] py-[3px] flex text-[13px] font-[500] rounded-[4px]">
+              {tool}
+            </div>
+          )
         })}
       </div>
       <div className="flex justify-between h-fit items-center mt-3 py-2 px-5">
         <h1 className="text-black text-[20px] font-[700] ">{name}</h1>
-        <a className=" text-black underline px-5 py-1 h-fit rounded-[3px]" href={url}>Sobre</a>
-        <a className="bg-black text-white px-5 py-1 h-fit rounded-[3px]" href={url}>Acessar</a>
+        <a
+          className=" text-black underline px-5 py-1 h-fit rounded-[3px]"
+          href={url}
+        >
+          Sobre
+        </a>
+        <a
+          className="bg-black text-white px-5 py-1 h-fit rounded-[3px]"
+          href={url}
+        >
+          Acessar
+        </a>
       </div>
     </motion.div>
   )
