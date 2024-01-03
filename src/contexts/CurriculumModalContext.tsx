3bef7fdee1b4ebbd86ff2aa424f179bbe66fd814
Luci Lua <luci.lua81@gmail.com>
2024-01-02 23:13:42 -0300

    true -> false (modal curriculum initial state)

--- src/contexts/CurriculumModalContext.tsx
+++ src/contexts/CurriculumModalContext.tsx
@@ -5,7 +5,7 @@ import { createContext, useState } from "react"
 export const CurriculumModalContext = createContext<any>({})
 
 export function CurriculumModalContextProvider({ children }) {
-  const [useModal, setUseModal] = useState<boolean>(true)
+  const [useModal, setUseModal] = useState<boolean>(false)
 
   function toggleModalOpen() {
     setUseModal(!useModal)
