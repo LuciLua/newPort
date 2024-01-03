f236e54ad005755ae5ccb1af2bff6e7f5017ba30
Luci Lua <luci.lua81@gmail.com>
2024-01-02 22:28:01 -0300

    prettier

--- src/components/MoreText.tsx
+++ src/components/MoreText.tsx
@@ -1,31 +1,48 @@
-'use client'
+"use client"
 
-import { useEffect, useState } from "react";
-import { TfiMoreAlt } from "react-icons/tfi";
+import { useEffect, useState } from "react"
+import { TfiMoreAlt } from "react-icons/tfi"
 
-export function MoreText({ children, numOfLines, style }: { children: React.ReactNode, numOfLines?: number, style?: string}) {
+export function MoreText({
+  children,
+  numOfLines,
+  style,
+}: {
+  children: React.ReactNode
+  numOfLines?: number
+  style?: string
+}) {
+  const [num, setNum] = useState(2)
 
-    const [num, setNum] = useState(2)
+  // useEffect(() => {
+  // }, [num])
 
-    // useEffect(() => {
-    // }, [num])
+  function moreText() {
+    setNum(999)
+  }
+  console.log(num)
 
-    function moreText() {
-        setNum(999)
-    }
-    console.log(num)
-
-    return (
-        <div className={`${style}`}>
-           
-            {num != 2 ?
-                <p className={`line-clamp-[10] overflow-hidden font-[500] z-20 text-justify`}>{children}</p>
-                :
-                <>
-                 <p className={`line-clamp-[4] overflow-hidden font-[500] z-20 text-justify`}>{children}</p>
-                <TfiMoreAlt onClick={moreText} className="bg-[#ddd] rounded-[30px] cursor-pointer w-[40px] h-[20px] text-[#575757] my-2" />
-                </>
-            }
-        </div>
-    )
+  return (
+    <div className={`${style}`}>
+      {num != 2 ? (
+        <p
+          className={`line-clamp-[10] overflow-hidden font-[500] z-20 text-justify`}
+        >
+          {children}
+        </p>
+      ) : (
+        <>
+          <p
+            className={`line-clamp-[4] overflow-hidden font-[500] z-20 text-justify`}
+          >
+            {children}
+          </p>
+          <TfiMoreAlt
+            onClick={moreText}
+            className="bg-[#ddd] rounded-[30px] cursor-pointer w-[40px] h-[20px] text-[#575757] my-2"
+          />
+        </>
+      )}
+    </div>
+  )
 }
