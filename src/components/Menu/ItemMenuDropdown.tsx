79287247083c04cc1ea983e97b52d5016cbe24b0
Luci Lua <luci.lua81@gmail.com>
2024-01-09 11:02:30 -0300

    i18n: menu translated

--- src/components/Menu/ItemMenuDropdown.tsx
+++ src/components/Menu/ItemMenuDropdown.tsx
@@ -4,9 +4,11 @@ import { BiWorld } from "react-icons/bi"
 import { TfiAngleDown } from "react-icons/tfi"
 import { BtnToggleLocale } from "../Btns/BtnToggleLocale"
 import { useState } from "react"
+import { useTranslation } from "../../hooks/useTranslation"
 
 export function ItemMenuDropdown() {
   const [dropDownOpen, setDropDownOpen] = useState(false)
+  const { translations } = useTranslation()
 
   return (
     <>
@@ -23,19 +25,19 @@ export function ItemMenuDropdown() {
             <div className="absolute right-[20px] top-[25px] z-10 mt-2 w-fit origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none font-[500]">
               <div className="w-full" role="none">
                 <BtnToggleLocale
-                  className="w-full text-gray-700 block px-4 py-2 text-sm hover:bg-gray-50"
+                  className={`w-full text-gray-700 block px-4 py-2 text-sm hover:bg-gray-50 ${translations.current_lang === translations.lang_opt_1 && 'bg-[#ddd] text-[#505050] hover:bg-[#ddd]'}`}
                   lang={"en"}
-                  lang_label="English"
+                  lang_label={translations.lang_opt_1}
                 />
                 <BtnToggleLocale
-                  className="w-full text-gray-700 block px-4 py-2 text-sm hover:bg-gray-50"
+                  className={`w-full text-gray-700 block px-4 py-2 text-sm hover:bg-gray-50 ${translations.current_lang === translations.lang_opt_2 && 'bg-[#ddd] text-[#505050] hover:bg-[#ddd]'}`}
                   lang={"pt"}
-                  lang_label="Portuguese"
+                  lang_label={translations.lang_opt_2}
                 />
                 <BtnToggleLocale
-                  className="w-full text-gray-700 block px-4 py-2 text-sm hover:bg-gray-50"
+                  className={`w-full text-gray-700 block px-4 py-2 text-sm hover:bg-gray-50 ${translations.current_lang === translations.lang_opt_3 && 'bg-[#ddd] text-[#505050] hover:bg-[#ddd]'}`}
                   lang={"fr"}
-                  lang_label="French"
+                  lang_label={translations.lang_opt_3}
                 />
               </div>
             </div>
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,9 +2,11 @@
 
 import { ItemMenuDropdown } from "./ItemMenuDropdown"
 import { useMenuStateColor } from "../../hooks/useMenuStateColor"
+import { useTranslation } from "../../hooks/useTranslation"
 
 export function Menu() {
   const { menuStateColor } = useMenuStateColor()
+  const { translations } = useTranslation()
 
   return menuStateColor ? (
     <div className="w-full h-[70px] bg-[#000000e7] text-[#fff]  backdrop-blur-[12px] flex justify-center fixed top-0 z-40 transition-[0.3s]">
@@ -13,19 +15,19 @@ export function Menu() {
           <div className="flex flex-col justify-center leading-[20px]">
             <h1 className="text-[24px] font-black">LuciLua /{">"}</h1>
             <a href="#" className="text-[14px] font-medium">
-              Frontend Developer
+              {translations.menu.logo}
             </a>
           </div>
         </li>
         <li>
           <ul className="list-none xl:gap-10 gap-3 flex items-center w-fit h-full font-[600]">
             <div className="lg:flex gap-10 hidden text-[14px]">
-              <li>Homepage</li>
-              <li>Projects</li>
-              <li>About me</li>
+              <li><a href="#">{translations.menu.homepage}</a></li>
+              <li>{translations.menu.projects}</li>
+              <li>{translations.menu.about}</li>
             </div>
             <li className="bg-[#fff] text-[14px] text-[#000] px-[30px] py-[10px] font-[600] rounded-[3px] text-nowrap">
-              Talk with me
+              {translations.menu.primary_btn}
             </li>
             <ItemMenuDropdown />
           </ul>
@@ -39,19 +41,19 @@ export function Menu() {
           <div className="flex flex-col justify-center leading-[20px]">
             <h1 className="text-[24px] font-black">LuciLua /{">"}</h1>
             <a href="#" className="text-[14px] font-medium">
-              Frontend Developer
+              {translations.menu.logo}
             </a>
           </div>
         </li>
         <li>
           <ul className="list-none xl:gap-10 gap-3 flex items-center w-fit h-full font-[600]">
             <div className="lg:flex gap-10 hidden text-[14px]">
-              <li>Homepage</li>
-              <li>Projects</li>
-              <li>About me</li>
+              <li><a href="#">{translations.menu.homepage}</a></li>
+              <li>{translations.menu.projects}</li>
+              <li>{translations.menu.about}</li>
             </div>
             <li className="bg-[#262626] text-[14px] text-white px-[30px] py-[10px] font-[600] rounded-[3px] text-nowrap">
-              Talk with me
+              {translations.menu.primary_btn}
             </li>
             <ItemMenuDropdown />
           </ul>
